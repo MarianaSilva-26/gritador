72d1f63420a2eb3daef0fb91769745935b1729dd
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-19 18:54:47 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string qualquerCoisa;
+
+            
+            //<---Inicio(Digite)--->//
+            Console.Write("Digite qualquer coisa: ");
+            qualquerCoisa = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{qualquerCoisa}!!!".ToUpper());
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+        }
+    }
+}
